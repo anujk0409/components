e14bcb9937afc8aa4f4d23069a4d9370c16a469e
Anuj kumar <anujkumarmharaja@gmail.com>
2024-12-05 15:54:24 +0530

    adding button component

---
+++ practice-components/src/Components/Button.jsx
@@ -0,0 +1,9 @@
+export function Button({onClick , title})
+{ return <div className="flex justify-center pt-60 ">
+    <div className=" flex justify-center bg-pink hover:bg-darkPink border-solid border-2 rounded-xl w-44 h-12 text-xl font-bold  text-white  shadow-lg">
+    <button onClick={""}>{title}</button>
+</div>
+
+    </div>
+    
+}
---
+++ practice-components/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      pink : "#f27a8a",
+      darkPink : "#b31d3f",
+      white:"#fef2f3",
+    },
+  },
+  plugins: [],
+}
+
